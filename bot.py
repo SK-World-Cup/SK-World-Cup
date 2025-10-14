2bca7c402bba3a364fe4c1ffdb4b6c0f6d1376d2
SK-World-Cup <skworldcup1234@gmail.com>
2025-10-14 00:10:25 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -691,7 +691,7 @@ async def whos_your_daddy(ctx):
         "The mafia boss who thought it would be fun to point a gun at his nuts and play russian roulette.",
         "LavaDragon, in which was shitting on the toliet for over a day before giving one last *fart* and exploding.",
         "Percy Jackson, the one that killed Kaity reading for the first time in his life",
-        
+        "Moosehead, who thinks he's good at everything when in reality he's a bitch"
 ]
 
     ]
